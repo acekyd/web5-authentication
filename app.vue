7c048c00d606019a4e14d2f78c5795d74565aec9
Adewale Abati <acekyd01@gmail.com>
2024-01-09 15:24:56 +0100

    Display DID on profile

--- app.vue
+++ app.vue
@@ -176,6 +176,11 @@
             <div class="mt-6 border-t border-gray-100">
               <dl class="divide-y divide-gray-100">
                 <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                  <dt class="text-sm font-medium leading-6 text-gray-900">Your DID</dt>
+                  <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0
+                  overflow-hidden text-ellipsis hover:tex hover:overflow-visible hover:break-words">{{ loggedInUserDID }}</dd>
+                </div>
+                <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                   <dt class="text-sm font-medium leading-6 text-gray-900">Full name</dt>
                   <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ userProfile.data.firstName }} {{ userProfile.data.lastName }}</dd>
                 </div>
